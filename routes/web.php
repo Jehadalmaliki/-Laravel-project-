68851b20247cf8feeda9f2f6954bddd795c9e0a0
Jehad Almaliki <yourgitemail@example.com>
2022-04-03 22:45:15 +0300

    Routes  all the pages in the jobs project

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,45 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/Skill', function () {
+    return view('Skill');
+});
+Route::get('/profile', function () {
+    return view('profile');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/listJob', function () {
+    return view('listJob');
+});
+Route::get('/Experince', function () {
+    return view('Experince');
+});
+Route::get('/Eduction', function () {
+    return view('Eduction');
+});
+Route::get('/decjop', function () {
+    return view('decjop');
+});
+Route::get('/dashboardPro', function () {
+    return view('dashboardPro');
+});
+Route::get('/Course', function () {
+    return view('Course');
+});
+Route::get('/connect', function () {
+    return view('connect');
+});
+Route::get('/company', function () {
+    return view('company');
+});
+Route::get('/account', function () {
+    return view('account');
+});
+Route::get('/Aboutus', function () {
+    return view('Aboutus');
+});
