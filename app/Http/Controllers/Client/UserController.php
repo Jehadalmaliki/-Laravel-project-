192b5b3e88f38c8851274682dd82d664ae3f2b8c
Jehad Almaliki <yourgitemail@example.com>
2022-04-11 00:02:48 +0300

    faet: make logout auth

--- app/Http/Controllers/Client/UserController.php
+++ app/Http/Controllers/Client/UserController.php
@@ -99,7 +99,7 @@ class UserController extends Controller
         public function logout(){
 
             Auth::logout();
-            return redirect()->route('login');
+            return redirect()->route('loginn');
 
         }
 }
